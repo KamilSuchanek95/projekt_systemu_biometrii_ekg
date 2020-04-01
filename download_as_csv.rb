7c635e82c4e2143248bcf29291b42610215ca502
Kamil Suchanek <kamisuc671@student.polsl.pl>
2020-04-01 17:45:28 +0200

    poprawka RECORDS

--- download_as_csv.rb
+++ download_as_csv.rb
@@ -1,5 +1,5 @@
 
-f = File.open('/home/kamil/Pulpit/RECORDS','r')
+f = File.open('RECORDS','r')
 f.each_line { |person_record|
 str = person_record.strip
 command = 'rdsamp -r ecgiddb/' + str + ' -c -H -f 0 -t 20 -v -pS >' + str + '.csv'
