7b20544ed134ae001b5fc3aed09728e41aaf07d5
Kamil Suchanek <kamilsuchanek95@gmail.com>
2020-05-05 14:10:45 +0200

    podzielenie na zbiór uczący i testowy
    póki co, nie trzeba poprawiać algorytmu detekcji QRS, dla wielu prób wysegmentowane ramki prezentują się odpowiednio. Teraz muszę zobaczyć jak użyć sieci...

--- projekt_mio.m
+++ projekt_mio.m
@@ -2,18 +2,19 @@
 % 80<- R ->170
 % detekcja R
 clear;clc
-load('records with data from Physionet.mat');
-R = create_Rs_struct(r);
-f = create_frames_struct(r, R);
-%% 
+% load('records with data from Physionet.mat');
+% R = create_Rs_struct(r);
+% f = create_frames_struct(r, R);
+load('f r R load.mat');
+%% wizualne sprawdzenie wykrytych za³amków R
 dt = 0.002;
 t = 0:dt:dt*(numel(r.person_1.record_1.filtered)-1);
-for i = 1:numel(fieldnames(r))
+for i = 1:numel(fieldnames(r)) % osoby...
     person = sprintf('person_%i', i);
-    for j = 1:numel(fieldnames(r.(person)))
+    for j = 1:numel(fieldnames(r.(person))) % rekordy...
         record = sprintf('record_%i', j);
         figure('Position', [0 0 1000 600])
-        plot(t, r.(person).(record).filtered); hold on;
+        plot(t, r.(person).(record).filtered); hold on; % 
         plot(t(R.(person).(record)), r.(person).(record).filtered(R.(person).(record)), 'r*')
         str = sprintf('{r.%s.%s} continue? [enter]', person, record);
         input(str)
@@ -21,14 +22,27 @@ for i = 1:numel(fieldnames(r))
     end
 end
 
-
-
-%% przegladanie okienek
+%% przegladanie wysegmentowanych okienek
 for i = 1:numel(fieldnames(f))
     person = sprintf('person_%i', i);
     plot(f.(person))
+    str = sprintf('{f.%s} continue? [enter]', person);
+    input(str)
     close
 end
+
+%% podzia³ na zbiory uczacy i testowy
+test = struct(); train = struct();
+for i = 1:numel(fieldnames(f))
+    person = sprintf('person_%i', i);
+    num = size(f.(person), 2);
+    num2 = ceil(num/2); num1 = ceil(num/2) - 1;
+    test.(person) = [];
+    test.(person)(1:251, 1:num1) = f.(person)(:,1:num1);
+    train.(person)(1:251, 1:num2) = f.(person)(:,1:num2);
+end
+
+
 
 
 
